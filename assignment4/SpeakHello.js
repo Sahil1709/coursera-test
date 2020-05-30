30b85eb514fcb526baab23b5110b12234ede9cee
Sahil Gupta <sahilofficial17@gmail.com>
2020-05-30 20:35:54 +0530

    hello js

---
+++ assignment4/SpeakHello.js
@@ -0,0 +1,18 @@
+
+(function (window) {
+
+
+    var helloSpeaker = {};
+
+
+    var speakWord = "Hello";
+
+    helloSpeaker.speak = function (name) {
+      console.log(speakWord + " " + name);
+    }
+
+
+
+    window.helloSpeaker = helloSpeaker;
+
+})(window);
